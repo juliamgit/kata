7495afdf5d8c67456fbb1de350bb0a0e6cf0902f
Julia Harper <julia.harper@myob.com>
2017-03-31 10:29:24 +1100

    add TDD version of rockPaperScissors and tests

--- codecademyProjects/rockPaperScissors.js
+++ codecademyProjects/rockPaperScissors.js
@@ -4,3 +4,53 @@
 "use strict";
 
 module.exports = determineWinner;
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === 'bomb') {
+        return 'nobody can beat you!';
+    }
+    if(userChoice === computerChoice) {
+        return 'the game is tied';
+    };
+    if(userChoice === 'rock' && computerChoice === 'scissors') {
+        return 'you beat the computer!';
+    };
+    if(userChoice === 'scissors' && computerChoice === 'paper' ) {
+        return 'you beat the computer!';
+    };
+    if(userChoice === 'paper' && computerChoice === 'rock') {
+        return 'you beat the computer!';
+    };
+        return 'the computer wins!';
+};
+
+//
+// function determineWinner(userChoice, computerChoice) {
+//     if (userChoice === 'bomb'){
+//         return 'nobody can beat you!';
+//     };
+//     if(userChoice === computerChoice){
+//         return 'the game is tied';
+//     };
+//     if (userChoice === 'rock') {
+//         if (computerChoice === 'scissors') {
+//             return 'you win!';
+//         } else {
+//             return 'you lose!';
+//         }
+//     }
+//     if (userChoice === 'scissors') {
+//         if (computerChoice === 'paper') {
+//             return 'you win!';
+//         } else {
+//             return 'you lose!';
+//         }
+//     }
+//     if (userChoice === 'paper') {
+//         if (computerChoice === 'rock') {
+//             return 'you win!';
+//         } else {
+//             return 'you lose!';
+//         }
+//     }
+// }
--- codecademyProjects/rockPaperScissors.spec.js
+++ codecademyProjects/rockPaperScissors.spec.js
@@ -3,4 +3,55 @@
  */
 "use strict";
 
-const determineWinner = require("./rockPaperScissors.js");
+const determineWinner = require('./rockPaperScissors.js');
+
+
+describe(determineWinner, function() {
+
+    describe("when the user inputs the 'bomb' cheat", () => {
+        it("confirms the user choice is 'bomb'", () => {
+            expect(determineWinner("bomb")).toEqual('nobody can beat you!');
+        });
+    });
+
+    describe("when the result is a tie", () => {
+
+        it ("compares two choices of 'rock'", () => {
+            expect(determineWinner("rock", "rock")).toEqual('the game is tied');
+        });
+        it("compares two choices of 'scissors'", () => {
+            expect(determineWinner("scissors", "scissors")).toEqual('the game is tied');
+        });
+        it("compares two choices of 'paper'", () => {
+            expect(determineWinner("paper", "paper")).toEqual('the game is tied');
+        });
+    });
+
+    describe("when the user wins", () => {
+
+        it("compares 'rock' with 'scissors'", () => {
+           expect(determineWinner("rock", "scissors")).toEqual('you beat the computer!');
+        });
+
+        it("compares 'scissors' with 'paper'", () => {
+            expect(determineWinner("scissors", "paper")).toEqual('you beat the computer!');
+        });
+        it("compares 'paper' with 'rock'", () => {
+            expect(determineWinner("paper", "rock")).toEqual('you beat the computer!');
+        });
+    });
+
+    describe("when the computer wins", () => {
+        it("compares 'scissors' with 'rock'", () => {
+            expect(determineWinner("scissors", "rock")).toEqual('the computer wins!');
+        });
+        it("compares 'paper' with 'scissors'", () => {
+            expect(determineWinner("paper", "scissors")).toEqual('the computer wins!');
+        });
+        it("compares 'rock' with 'paper'", () => {
+            expect(determineWinner("rock", "paper")).toEqual('the computer wins!');
+        });
+    });
+
+});
+
